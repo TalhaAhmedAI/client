64840a74c30be5013ed78093d5900a96435a0b68
tester <aiforeveryoneai@gmail.com>
2020-08-19 14:25:53 +0500

    Implemented Google Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,13 @@ import StreamCreate from "./streams/streamCreate";
 import StreamDelete from "./streams/streamDelete";
 import StreamEdit from "./streams/streamEdit";
 import StreamShow from "./streams/streamShow";
+import Header from "./header";
 
 function App() {
   return (
-    <div>
-      <h1>Header!</h1>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/edit" component={StreamEdit} />
---
+++ src/components/googleAuth.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "10580895132-m6ou6kbt1qm9271j92ddvh628g63lnnf.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthStatus() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+  render() {
+    return <div>{this.renderAuthStatus()}</div>;
+  }
+}
+
+export default GoogleAuth;
