7e66ac0e13457052500c9f711445673b47ab10ac
tester <aiforeveryoneai@gmail.com>
2020-08-20 08:20:53 +0500

    Added Redux for RESTful Services
    Created action creators, reducer for RESTful Services

--- src/components/streams/streamCreate.js
+++ src/components/streams/streamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class CreateStream extends React.Component {
   renderError({ touched, error }) {
@@ -22,9 +24,9 @@ class CreateStream extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
   render() {
     return (
       <form
@@ -54,7 +56,10 @@ const validate = (formValues) => {
   }
   return errors;
 };
-export default reduxForm({
+
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(CreateStream);
+
+export default connect(null, { createStream })(formWrapped);
