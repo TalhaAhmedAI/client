22e52025d8af448929de8a9a15f599664ec5b8bb
tester <aiforeveryoneai@gmail.com>
2020-08-24 07:57:57 +0500

    streamEdit completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,6 +18,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: "CREATE_STREAM", payload: response.data });
+  // history.pushState('/')
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -33,9 +34,10 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`streams/${id}`, formValues);
+  const response = await streams.patch(`streams/${id}`, formValues);
 
   dispatch({ type: "EDIT_STREAM", payload: response.data });
+  // history.pushState('/')
 };
 
 export const deleteStream = (id) => async (dispatch) => {
